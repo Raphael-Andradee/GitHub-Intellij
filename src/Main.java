b43adfe9adeabddebecac9ca627ed7f44d12a900
Raphael <163064252+Raphael-Andradee@users.noreply.github.com>
2025-06-11 14:41:01 +0100

    condicionais first commit from intellij

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class Main {
+    public static void main(String[] args) {
+  Locale.setDefault(Locale.UK);
+  Scanner sc = new Scanner(System.in);
+
+ int horas;
+ System.out.println("Que horas?");
+ horas = sc.nextInt();
+ if(horas<12){
+   System.out.println("Bom dia");
+ }
+ else if (horas<18) {
+   System.out.println("Boa Tarde");
+
+ }
+ else{System.out.println("Boa noite");}
+
+
+   sc.close();
+    }
+}
